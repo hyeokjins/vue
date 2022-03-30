b9c6c8b826164522bef18dc054edc5b1f29323e0
hyeokjins <yhg1368@gmail.com>
2022-03-30 13:35:15 +0900

    Revert "♻️ refactoring"
    This reverts commit 1f2ee34921db779bb64b87b78f5a10ab8f919cf2.

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -1,13 +1,29 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueRouter from "vue-router";
-import router from "./routes";
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router';
+import MainPage from './pages/MainPage.vue';
+import LoginPage from './pages/LoginPage.vue';
+import ProfilePage from './pages/ProfilePage.vue';
 
-Vue.config.productionTip = false;
+
+Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: MainPage },
+  { path: '/login', component: LoginPage },
+  { path: '/profile', component: ProfilePage }
+];
+
+const router = new VueRouter({
+  routes,
+  MainPage,
+  LoginPage,
+  ProfilePage
+});
+
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
