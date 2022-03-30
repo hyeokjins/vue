6247450bf32cebf436484fbac103ecd87717badf
hyeokjins <yhg1368@gmail.com>
2022-03-30 14:07:41 +0900

    🔒 login profile #9

--- part2/ch03/src/pages/ProfilePage.vue
+++ part2/ch03/src/pages/ProfilePage.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <h2>Profile Page</h2>
+  <div class="profile">
+    <h1>Profile</h1>
+    <div>{{ email }}</div>
+    <button @click="logout">log out</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      email: localStorage.getItem('email')
+    }
   },
-};
+  methods: {
+    logout() {
+      localStorage.removeItem('token')
+      localStorage.removeItem('email')
+      this.$router.push('/')
+    }
+  }
+}
 </script>
